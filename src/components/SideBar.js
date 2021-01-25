cfbe492ca0a0ef036c534595092fb2d5798a96f1
youngsuk-kim <thayer2392@gmail.com>
2021-01-25 16:17:31 +0900

    사이드바 개발

--- src/components/SideBar.js
+++ src/components/SideBar.js
@@ -6,29 +6,58 @@ import ListItemText from "@material-ui/core/ListItemText";
 import IconButton from "@material-ui/core/IconButton";
 import MenuIcon from "@material-ui/icons/Menu";
 import { makeStyles } from "@material-ui/core/styles";
+import ListItemIcon from "@material-ui/core/ListItemIcon";
+import Divider from "@material-ui/core/Divider";
+import Collapse from "@material-ui/core/Collapse";
+import IconExpandLess from "@material-ui/icons/ExpandLess";
+import IconExpandMore from "@material-ui/icons/ExpandMore";
+import IconLibraryBooks from "@material-ui/icons/LibraryBooks";
 
 const useStyles = makeStyles((theme) => ({
   menuButton: {
     marginRight: theme.spacing(2),
   },
+  menuItemIcon: {
+    color: "#ff8a80",
+    display: "flex",
+    justifyContent: "center",
+  }
 }));
 
 export default function SideBar() {
   const [open, setOpen] = useState(false);
+  const [navMenuOpen, setNavMenuOpen] = useState(false);
+
   const classes = useStyles();
 
   function toggleDrawer() {
     setOpen(!open);
   }
+  function handleClick() {
+    setNavMenuOpen(!navMenuOpen);
+  }
 
   const sidebarList = (
-    <div>
-      <List>
-        <ListItem button key={"category"}>
-          <ListItemText primary={"책 카테고리"} />
-        </ListItem>
-      </List>
-    </div>
+    <List component="nav">
+      <ListItem button onClick={handleClick} className={classes.menuItem}>
+        <ListItemIcon className={classes.menuItemIcon}>
+          <IconLibraryBooks />
+        </ListItemIcon>
+        <ListItemText className={classes.listItemIcon} primary="책 카테고리" />
+        {navMenuOpen ? <IconExpandLess /> : <IconExpandMore />}
+      </ListItem>
+      <Collapse in={navMenuOpen} timeout="auto" unmountOnExit>
+        <Divider />
+        <List component="div" disablePadding>
+          <ListItem button className={classes.menuItem}>
+            <ListItemText primary="Nested Page 1" />
+          </ListItem>
+          <ListItem button className={classes.menuItem}>
+            <ListItemText primary="Nested Page 2" />
+          </ListItem>
+        </List>
+      </Collapse>
+    </List>
   );
 
   return (
